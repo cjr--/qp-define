2f44de3edf65af29ab5b6ac8e35ba0f8aaaaf7ec
cjr-- <cjr@hotmail.co.uk>
2018-11-01 13:12:08 +0000

    build changes

--- build.js
+++ build.js
@@ -1,6 +1,9 @@
 var path = require('path');
 var fs = require('fs');
 var uglify = require('uglify-js');
+var term = require('qp-library/term');
+
+term.set_title('qp-define - build');
 
 fs.writeFileSync(
   path.join(__dirname, 'qp-define.min.js'),
